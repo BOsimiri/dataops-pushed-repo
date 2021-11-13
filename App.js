5e873a404881ea69ca5eac89dc94dbe714adb2c2
BOsimiri <osimiriblessing@gmail.com>
2021-11-13 17:07:47 +0000

    Revert "Added additional logo"
    This reverts commit f8f32cd9aed51e37c343674e9aca685182bbd011.

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import logo from './logo.svg';
-import logo from './AdditionalLogo.svg';
 import './App.css';
 
 function App() {
@@ -7,7 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-	<img src={AdditionalLogo} className="App-AdditionalLlogo" alt="AdditionalLogo" />
         <p>
           I CHANGED THE MESSAGE!
         </p>
