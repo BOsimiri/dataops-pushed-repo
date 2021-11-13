f8f32cd9aed51e37c343674e9aca685182bbd011
BOsimiri <osimiriblessing@gmail.com>
2021-11-13 11:01:49 +0000

    Added additional logo
    Added additional logo

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import logo from './AdditionalLogo.svg';
 import './App.css';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+	<img src={AdditionalLogo} className="App-AdditionalLlogo" alt="AdditionalLogo" />
         <p>
           I CHANGED THE MESSAGE!
         </p>
